5f6b46b0eac23b13d67211479c8651c7449bc75f
carolina.cepeda-v <carolina.cepeda-v@mail.escuelaing.edu.co>
2026-02-17 00:18:15 -0500

    fix: control de versiones

--- src/Punto4/FunServer.java
+++ src/Punto4/FunServer.java
@@ -11,7 +11,7 @@ public class FunServer {
 
     private static final int PORT = 35000;
 
-    public static void main(String[] args) {
+    public static void main(String[] args ){
 
         System.out.println("Servidor iniciado en el puerto " + PORT);
 
